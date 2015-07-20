78f2761d7572d4702ff18f5b8b80cfe483fd7eb4
Benjamin M. Brown <benjaminbb@gmail.com>
2015-07-20 15:10:45 -0500

    adding 'finishable' todos and inline style

--- app.ts
+++ app.ts
@@ -10,32 +10,27 @@ import {Component, Template, View, bootstrap, NgFor, NgIf} from 'angular2/angula
 
 class TodoAppComponent{ 
 	todos: Array<string>;
-
 	name: string;
-	//todoStore: TodoStore;
 
 	constructor(){
-		this.name = 'ben';
+		this.name = 'Ben';
 		this.todos = [{ text: 'Buy mom flowers', done: false }, { text: 'Grab bulls tix', done: false }, { text: 'Eat food', done: false }]; 
 	}
  
 	addTodo(todo:string){
 		this.todos.unshift({ text: todo, done: false });
-
 	}
 
 	toggleTodoState(todo){
-	
 		todo.done = !todo.done;
-	
 	}
+	
 	doneTyping($event){
 		if($event.which ===13){
 			this.addTodo($event.target.value);
 			$event.target.value = null;
 		}
 	}
-
 }
 
 bootstrap(TodoAppComponent);
